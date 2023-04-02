7762c467ae2ff6094e0232da676f81bb401ebc1d
shubhampachghare <shubhampachghare@gmail.com>
2023-04-02 15:44:31 +0530

    [Shubham] -DeckOfCards

---
+++ Day 11  12 Object Oriented Practice Problem/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11__12_Object_Oriented_Practice_Problem
+{
+    public class Card
+    {
+        private string face;
+        private string suit;
+
+        public Card(string cardFace, string cardSuit)
+        {
+            face = cardFace;
+            suit = cardSuit;
+        }
+
+
+        public override string ToString()
+        {
+            return face + " of " + suit;
+        }
+    }
+}
---
+++ Day 11  12 Object Oriented Practice Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day_11__12_Object_Oriented_Practice_Problem
+{
+    public class Example
+    {
+        public static void Main(string[] args)
+        {
+            Deck deck1 = new Deck();
+            deck1.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Console.Write("{0,-19}", deck1.DealCard());
+                if ((i + 1) % 4 == 0)
+                    Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
